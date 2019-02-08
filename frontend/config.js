49426d9880f5e14bd6faec88500519005c85bb4e
root <jrohit072@gmail.com>
2019-02-08 06:08:12 +0000

    first commit

---
+++ frontend/config.js
@@ -0,0 +1,3 @@
+// This is client side config only - don't put anything in here that shouldn't be public!
+export const endpoint = `https://8080-dot-6319680-dot-devshell.appspot.com/`;
+export const perPage = 4;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+class Sell extends Component{
+    render(){
+        return <h1>Cool sell</h1>
+    }
+}
+export default Sell;
